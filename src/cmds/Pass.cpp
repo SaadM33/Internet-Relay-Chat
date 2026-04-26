ed0c1935070fd7bc853a7d03711c0517000cb403
Hatim Qannouch <hqannouc@c2r7p8.1337.ma>
2026-04-27 00:28:08 +0100

    Fixed wrong password in PASS

--- src/cmds/Pass.cpp
+++ src/cmds/Pass.cpp
@@ -14,6 +14,14 @@ void	Server::execPass(int fd, Message msg)
 
 	if (msg.params[0] != this->passwd) {
 		sendReply(fd, ERR_PASSWDMISMATCH);
+		for (size_t i = 0; i < poll_fds.size(); i++)
+		{
+			if (fd == poll_fds[i].fd)
+			{
+				this->disconnectClient(i, fd);
+				break ;
+			}
+		}
 		return ;
 	}
 	this->clients[fd]->has_pass = true;
